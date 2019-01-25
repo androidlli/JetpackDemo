16b4b8c2bb711d9e5833498731785962abdd1cfd
lili <lili92823@163.com>
2019-01-25 17:22:42 +0800

    写了application类

---
+++ app/src/main/java/com/cango/basicdemo/AppExcutors.kt
@@ -0,0 +1,28 @@
+package com.cango.basicdemo
+
+import android.os.Handler
+import android.os.Looper
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+
+/**
+ * <pre>
+ *     author : cango
+ *     e-mail : lili92823@163.com
+ *     time   : 2019/01/25
+ *     desc   :
+ * </pre>
+ */
+class AppExecutors {
+    val mDiskIO = Executors.newSingleThreadExecutor()
+    val mNetworkIO = Executors.newFixedThreadPool(3)
+    val mMainThread = MainThreadExecutor()
+
+}
+
+class MainThreadExecutor : Executor {
+    private val mMainHandler = Handler(Looper.getMainLooper())
+    override fun execute(command: Runnable?) {
+        mMainHandler.post(command)
+    }
+}
---
+++ app/src/main/java/com/cango/basicdemo/BasicApp.kt
@@ -0,0 +1,17 @@
+package com.cango.basicdemo
+
+import android.app.Application
+
+/**
+ * <pre>
+ *     author : cango
+ *     e-mail : lili92823@163.com
+ *     time   : 2019/01/25
+ *     desc   :
+ * </pre>
+ */
+class BasicApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
